de3cd136cad29116b017968125914cbaa18a0caa
Halasalhab <halasalhab0@gmail.com>
2021-01-16 21:28:19 +0200

    after heroku

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aatt%p*k9+#1(*^gy_lhd01^q)%+x!sn!k21^7fm@ztq*5d5st'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['halaportfolio.herokuapp.com']
 
 
 # Application definition
@@ -135,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
